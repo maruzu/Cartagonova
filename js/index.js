cf59ec47639a4fc9d10505469bcf3154db137442
mariobp <mariobarrpach@gmail.com>
2016-11-13 03:58:28 -0500

    menu dragable

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 /*
   Esta linea permite que salga sideNav con el menu movil
 */
-$(".button-collapse").sideNav();
+$(".button-collapse").sideNav({
+    menuWidth: 240, // Default is 240
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+});
+
 $(".dropdown-button").dropdown();
 
 /*
